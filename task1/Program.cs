8c9c60b7b2a413d06a702e33ec500ad70c0f50a7
hatorji <Davydov.g@icloud.com>
2024-06-05 01:57:37 +0300

    task1:черновое рабочее  решение

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+using System.Runtime.InteropServices.Marshalling;
+
+void AllNaturalNumbers(int m, int n)
+{
+    if (m > n) // ветвление создано, чтобы сохранить последовательность не зависимо какое число больше
+    {
+        if (m == n) return; 
+        Console.Write($"{m} ");
+        AllNaturalNumbers(m - 1, n);       
+    }
+    else
+    {
+        if (n == m) return;
+        AllNaturalNumbers(m, n - 1);
+        Console.Write($"{n} ");
+    }
+}
+
+int startM = 1;
+int finishN = 9;
+if(startM > finishN) // Без добавления "-1" к границе отрезка рекурсия съедала последнее число в переборе
+    {
+        AllNaturalNumbers(startM, finishN - 1);
+    }
+else
+    {
+        AllNaturalNumbers(startM - 1, finishN);
+    }
